032ed9eeb166ff4b6b8894bb6df8ff09de42f57d
0000xFFFF <coldmailgg@gmail.com>
2025-08-15 01:49:58 +0200

    update pip

--- setup_pvenv_amd.sh
+++ setup_pvenv_amd.sh
@@ -3,3 +3,4 @@ python -m venv venv
 source venv/bin/activate
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.4
 pip install -r requirements_newest.txt
+pip install --upgrade pip
