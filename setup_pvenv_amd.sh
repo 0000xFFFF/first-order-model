3695847bbf0432c6420f78dd3401827d8d9ec086
0000xFFFF <coldmailgg@gmail.com>
2025-08-15 01:42:25 +0200

    new torch version

--- setup_pvenv_amd.sh
+++ setup_pvenv_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python -m venv venv
 source venv/bin/activate
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.4
 pip install -r requirements_newest.txt
